e75f6a619ad5193cd327bcf6af8f42365d344e70
sanagif <sanagif@vivaldi.net>
2020-04-15 01:35:22 +0900

    add: Fibonacci numbers

--- Hello.hs
+++ Hello.hs
@@ -1,2 +1,10 @@
+import Prelude
+
+fib :: Integer -> Integer
+fib n = fib' n 1 0
+  where
+    fib' 0 a _ = a
+    fib' n a b = fib' (n - 1) (a + b) a
+
 main :: IO ()
-main = putStrLn "Hello World"
+main = putStrLn $ "Hello World " ++ (show $ fib 10)
